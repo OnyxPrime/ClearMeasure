2d64c97921227bde0654874096fe313cd8658ae3
Ryan Overton <ryan.overton@outlook.com>
2024-06-18 23:15:46 -0500

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp2
+{
+    using ClassLibrary1;
+    internal class Program
+    {
+
+        public class MyValue
+        {
+            public string Value { get; set; } = "";
+
+            public  MyValue(string value)
+            {
+                Value = value;
+            }
+
+            public override string ToString()
+            {
+                return Value; 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var results = Class1.GetNumbers(short.MaxValue, [(3, "Ryan"), (5, "Overton")]);
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
